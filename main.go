c864290dd28f723cdcbf8e74b6e8243f4ecbd0ea
Sergey Simonov <gordenholf@gmail.com>
2026-05-29 08:45:04 +0300

    1.0

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +15,102 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 {
+		log.Println("Передан размер слайса меньше или равный нулю. Дальнейшее выполнение программы некорректно.")
+		return nil
+	}
+	sliceInteger := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		rnd := rand.Intn(size)
+		sliceInteger[i] = rnd + 1 //  задание требует целые положительные числа
+
+	}
+
+	return sliceInteger
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if data == nil {
+		log.Println("Попытка вычесть максимум из nil слайса. Дальнейшее выполнение программы некорректно.")
+		return 0
+	}
+	if len(data) == 0 {
+		log.Println("Попытка вычесть максимум из пустого слайса. Дальнейшее выполнение программы некорректно.")
+		return 0
+	}
+
+	maxNumberOfRandom := data[0] // так корректнее, мне кажется.
+
+	for _, v := range data {
+		if maxNumberOfRandom < v {
+			maxNumberOfRandom = v
+		}
+	}
+
+	return maxNumberOfRandom
+
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	sliceMaxSlice := []int{}        // слайс для хранения максимальных чисел для дальнейшего перебора
+	lenSlice := len(data)           // длина слайса
+	valueSlice := lenSlice / CHUNKS // размер слайса
+
+	for i := 0; i < 8; i++ {
+
+		startSlice := i * valueSlice        // начальный индекс
+		endSlice := startSlice + valueSlice // конечный индекс
+
+		wg.Add(1)
+		go func(start, end int) {
+			defer wg.Done()
+
+			max := data[0]
+
+			for _, v := range data[start:end] {
+				if max < v {
+					max = v
+				}
+			}
+			mu.Lock() // блокируем критическую секцию кода
+			sliceMaxSlice = append(sliceMaxSlice, max)
+			mu.Unlock() // разблокируем критическую секцию кода
+		}(startSlice, endSlice)
+
+	}
+	wg.Wait()
+	return maximum(sliceMaxSlice)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	// в задаче сказано "Время нахождения максимумов измеряйте в Микросекундах.",
+	// однако в претесте на github в выводе указаны "ms", изменил на "us".
+	// Также в претесте некорректно используются "\n" (в printf нет переноса), поправил для лучшего вывода.
+	// https://github.com/Yandex-Practicum/go1fl-sprint9-final-tpl/blob/main/main.go - если нужно для проверки.
+
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	sliceRand := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	timeStart := time.Now()
+	max := maximum(sliceRand)
+	times := time.Since((timeStart))
+	elapsed := times.Microseconds()
+	// можно обойтись без переменной заменив elapsed на "times.Microseconds()" в принте,
+	//  но т.к. в задаче стоит elapsed решил просто добавить переменную.
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d us\n", max, elapsed)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
-
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	timeStart = time.Now()
+	max = maxChunks(sliceRand)
+	times = time.Since(timeStart)
+	elapsed = times.Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d us\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,62 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		input    int
+		expected int
+	}{
+		{"проверяем: 1", 1, 1},
+		{"проверяем: 100", 100, 100},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := generateRandomElements(tt.input)
+			assert.Len(t, result, tt.expected)
+		})
+	}
+
+	assert.Nil(t, generateRandomElements(0))
+	assert.Nil(t, generateRandomElements(-1))
+	assert.NotNil(t, generateRandomElements(1))
+
+	for _, v := range generateRandomElements(100) {
+		assert.Greater(t, v, 0)
+	}
+
+}
+
+func TestMaximum(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{"слайс с отрицательными числами макимум: -1", []int{-100, -50, -2, -1}, -1},
+		{"корректный слайс с одним эллементом, максимум: 99", []int{99}, 99}, //корректно, т.к. мы берем за нулевой к тесту его,
+		//а так, как он не больше самого себя цикл просто завершается
+		{"корректный слайс, максимум: 100", []int{1, 2, 3, 4, 5, 100, 9}, 100},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := maximum(tt.input)
+			assert.Equal(t, tt.expected, result)
+
+		})
+
+	}
+
+	assert.Equal(t, 0, maximum(nil))
+	assert.Equal(t, 0, maximum([]int{}))
+
+}
